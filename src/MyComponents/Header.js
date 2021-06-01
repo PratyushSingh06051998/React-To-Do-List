bdaa2b72248074c08a78ae99c98a6aba11d23706
PratyushSingh06051998 <pratyushsingh.6may@gmail.com>
2021-06-01 19:41:23 +0530

    .

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -6,7 +6,7 @@ export default function  Header(props) {
     return (
         <div>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
+        <div class="container-fluid"> 
           <a class="navbar-brand" href="#">{props.title}</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
